4d70597f6d2d694bdd8b13d5bbe7a616ee530c6e
handriss <andrashinkel@gmail.com>
2016-08-23 01:02:28 +0200

    Basemodel implemented

---
+++ models/basemodel.py
@@ -0,0 +1,12 @@
+class BaseModel(Model):
+    """A base model that will use our Postgresql database"""
+
+    @classmethod
+    def get_model_attributes(cls):
+        """ Returns the model's attributes
+        :returns: list -- a list of strings of the attributes
+        """
+        return cls._meta.sorted_field_names
+
+    class Meta:
+        database = db
