45124c6aed7e0b72fdf7571ac708f33d1202c9c3
kalmeee <revkalman@gmail.com>
2016-08-31 14:18:36 +0200

    merge ready

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 from flask import Flask, render_template
-
+from models import Applicant
+from populator import Populator
 
 app = Flask(__name__)
+app.config['DEBUG'] = True
 
 
 @app.route('/')
@@ -9,5 +11,14 @@ def index():
     return render_template('index.html')
 
 
+@app.route('/confirm_page')
+def confirm():
+    Applicant.get_application_codes()
+    code = Applicant.application_code_generator()
+    return render_template('confirm_page.html', code=code)
+
+
 if __name__ == "__main__":
+    Populator.establish_connection()
+    Populator.populate_tables()
     app.run()
--- models/applicant.py
+++ models/applicant.py
@@ -2,6 +2,8 @@ from models.basemodel import BaseModel
 from peewee import *
 from models.school import School
 from models.interviewslot import InterviewSlot
+import random
+import string
 
 
 class Applicant(BaseModel):
@@ -13,3 +15,41 @@ class Applicant(BaseModel):
     status = CharField(default='New applicant')
     school = ForeignKeyField(School, related_name='applicant', null=True)
     interviewslot = ForeignKeyField(InterviewSlot, null=True, related_name='applicants')
+
+    @classmethod
+    def get_application_codes(cls):
+        """ Saves to the class and returns all application codes of the applicants. """
+        cls.application_codes = [applicant.application_code for applicant in Applicant.select()]
+        return cls.application_codes
+
+
+    @classmethod
+    def application_code_generator(cls):
+        """ Generates a new, random, six-digit application code that is unique to other application codes. """
+        application_code = (''.join(random.choice(string.ascii_uppercase + string.digits) for i in range(6)))
+        while application_code in cls.application_codes:
+            application_code = (''.join(random.choice(string.ascii_uppercase + string.digits) for i in range(6)))
+        return application_code
+
+    @classmethod
+    def assign_application_code(cls):
+        print("valami")
+        """ Assigns application code to those applicants who do not have one and returns them in a list. """
+        applicants = []
+        for applicant in cls.select().where(cls.application_code >> None):
+            applicant.application_code = cls.application_code_generator()
+            applicant.save()
+            applicants.append([
+                    applicant.first_name, applicant.last_name,
+                    applicant.application_code, applicant.school.city, applicant.email
+                ])
+        print(applicants)
+        return applicants
+
+@staticmethod
+def assign_school():
+    """ Assigns a school to those applicants who do not have one. """
+    for applicant in Applicant.select().where(Applicant.school >> None):
+        city = City.get(City.city == applicant.city).school_city
+        applicant.school = School.get(School.city == city).id
+        applicant.save()
