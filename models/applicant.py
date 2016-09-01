a1b5e121fbc2f55b3d9d46d5b71d352b230d991c
annakertesz <kerteszannanak@gmail.com>
2016-09-01 15:59:05 +0200

    user storties are ready

--- models/applicant.py
+++ models/applicant.py
@@ -2,6 +2,7 @@ from models.basemodel import BaseModel
 from peewee import *
 from models.school import School
 from models.interviewslot import InterviewSlot
+from models.city import City
 
 
 
@@ -26,3 +27,33 @@ class Applicant(BaseModel):
         return query
 
 
+
+    @classmethod
+    def run_query(cls, filter_by, value):
+
+        # mentor query for the future
+
+        # if filter_by == "mentor":
+        #     query = []
+        #     for applicant in Applicant.select():
+        #         if applicant.interview.mentor.first_name.contains(value):
+        #             query.append([applicant.first_name, applicant.last_name, applicant.email, applicant.city])
+
+
+        if filter_by == "school":
+            query = Applicant.select(
+                Applicant.first_name,
+                Applicant.last_name,
+                Applicant.email,
+                Applicant.city
+            ).join(City, JOIN.FULL, Applicant.city == City.city).where(City.school_city.contains(value))
+        else:
+            query = cls.select(cls.first_name,
+                               cls.last_name,
+                               cls.email,
+                               cls.city,
+                               cls.application_code,
+                               School.city,
+                               cls.status).join(School, JOIN.LEFT_OUTER).where(getattr(cls, filter_by).contains(value))
+
+        return query
--- server.py
+++ server.py
@@ -10,25 +10,34 @@ def apply():
 
 @app.route('/save', methods=['POST'])
 def post():
-    print('ghjk')
     new_applicant = {}
-    # new_applicant.append(request.form['first_name'])
     new_applicant['first_name'] = request.form['first_name']
     new_applicant['last_name'] = request.form['last_name']
     new_applicant['email'] = request.form['email']
     new_applicant['city'] = request.form['city']
     Applicant.new_applicant(new_applicant)
-    return redirect('/list')
+    return redirect('/list_menu')
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
-@app.route('/list', methods=['GET'])
+@app.route('/list_menu', methods=['GET'])
 def send():
     query_to_print = Applicant.all_applicant()
     print (query_to_print)
-    return render_template('list.html', query=query_to_print)
+    return render_template('list_menu.html', query=query_to_print)
+
+
+
+
+@app.route('/filtered_list', methods= ['GET', 'POST'])
+def run_query():
+    filter_by = request.form['filter_by']
+    value = request.form['value']
+    query_to_print = Applicant.run_query(filter_by, value)
+    return render_template('list_menu.html', query=query_to_print)
+
 
 
 app.run()
