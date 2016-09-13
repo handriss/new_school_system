0a57fd3dbb752a914ef64198c0994f8fec58d487
handriss <andrashinkel@gmail.com>
2016-09-13 13:35:40 +0200

    basic jquery example working

---
+++ static/js/toggle.js
@@ -0,0 +1,10 @@
+$('#toggle_message').click(function() {
+    var value = $('#toggle_message').attr('value');
+    $('#message').toggle('fast');
+
+    if (value == 'Hide'){
+        $('#toggle_message').attr('value', 'Show');
+    } else if (value == 'Show'){
+        $('#toggle_message').attr('value', 'Hide')
+    }
+});
