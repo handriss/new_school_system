af4a0c30feb35aefc16936b6d7c9d1343671bd59
kalmeee <revkalman@gmail.com>
2016-09-13 11:33:24 +0200

    peewee stuff working as intended

--- server.py
+++ server.py
@@ -13,17 +13,17 @@ def apply():
 
 @app.route('/confirm_page', methods=['POST'])
 def post():
-    new_applicant = {}
-    # new_applicant.append(request.form['first_name'])
+
     Applicant.get_application_codes()
     code = Applicant.application_code_generator()
-    new_applicant['first_name'] = request.form['first_name']
-    new_applicant['last_name'] = request.form['last_name']
-    new_applicant['email'] = request.form['email']
-    new_applicant['city'] = request.form['city']
-    new_applicant['code'] = code
-    Applicant.new_applicant(new_applicant)
-
+    Applicant.create(
+        first_name=request.form['first_name'],
+        last_name=request.form['last_name'],
+        email=request.form['email'],
+        city=request.form['city'],
+        application_code=code
+    )
+    new_applicant = Applicant.get(application_code=code)
     return render_template('confirm_page.html', new_applicant=new_applicant)
 
 
@@ -31,16 +31,13 @@ def post():
 def index():
     return render_template('index.html')
 
+
 @app.route('/list_menu', methods=['GET'])
 def send():
-    query_to_print = Applicant.all_applicant()
-    print (query_to_print)
-    return render_template('list_menu.html', query=query_to_print)
+    return render_template('list_menu.html', query=Applicant.select())
 
 
-
-
-@app.route('/filtered_list', methods= ['GET', 'POST'])
+@app.route('/filtered_list', methods=['GET', 'POST'])
 def run_query():
     filter_by = request.form['filter_by']
     value = request.form['value']
